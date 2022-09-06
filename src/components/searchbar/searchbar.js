76363e2ae3362854b915dc5c1e0f25134dd8b8f5
SssngM <rarang222@gmail.com>
2022-09-06 15:37:35 -0700

    fatch and update

--- src/components/searchbar/searchbar.js
+++ src/components/searchbar/searchbar.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import "./searchbar.css"
+import { useState } from "react";
+import Booklist from "../booklist/booklist"
 
-function Searchbar() {
+
+function Searchbar(props) {
+const [book, setBook] = useState('');
+
+
+
+const handleSubmit = event => {
+    event.preventDefault();
+}
+
   return (
     <div> 
-     <form>
-        <input
+     <form onSubmit={handleSubmit}>
+        {/* <Booklist input={book} */}
+            <input
             type ="text"
             placeholder="Search your book"
+            onChange={event=> setBook(event.target.value)}
+            value={book}
         /> 
         <button type="submit">Search</button>
+        {/* <Booklist/> */}
      </form>
     </div>
   )
